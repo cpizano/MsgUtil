875bcea76e863f8b265b817d83715b01ac26baa8
Carlos Pizano <cpu@chromium.org>
2014-08-31 11:03:54 -0700

    main file and VS files
    currently 32 bit build configurations.

---
+++ msgutil.cpp
@@ -0,0 +1,15 @@
+// msgutil.cpp : tool to send windows messages.
+
+#include <SDKDDKVer.h>
+
+#include <stdio.h>
+#include <tchar.h>
+#include <Windows.h>
+
+#include <stdlib.h>
+
+
+int wmain(int argc, wchar_t* argv[]) {
+
+  return 0;
+}
